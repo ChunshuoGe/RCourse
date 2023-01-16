4ae6f2717275ffa16c8ea59ab9f717c2b03305c6
Chunshuo Ge <Chunshuo@chalmers.se>
2023-01-16 16:09:41 +0100

    add comments for practice

---
+++ R/learning.R
@@ -0,0 +1,20 @@
+10
+10
+10
+
+# R basics ----------------------------------------------------------------
+weight_kilos <- 100
+wtight_kilos <- 10
+weight_kilos
+colnames(airquality)
+str(airquality)
+summary(airquality)
+2 + 2
+2 + 2
+2 + 2
+
+# packages ----------------------------------------------------------------
+
+library(tidyverse)
+
+tsasdsf
