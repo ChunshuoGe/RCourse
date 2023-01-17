c5ca88b9c4d47fc62a45386e3a10d3788f49854c
Chunshuo Ge <Chunshuo@chalmers.se>
2023-01-17 11:15:48 +0100

    Made smaller nahanes dataset

--- R/learning.R
+++ R/learning.R
@@ -1,8 +1,3 @@
-This is an example of a conflict.
-10
-10
-10
-
 # R basics ----------------------------------------------------------------
 weight_kilos <- 100
 wtight_kilos <- 10
@@ -10,10 +5,34 @@ weight_kilos
 colnames(airquality)
 str(airquality)
 summary(airquality)
-2 + 2
-2 + 2
-2 + 2
 
 # packages ----------------------------------------------------------------
-
 library(tidyverse)
+library(NHANES)
+
+# Looking at data ---------------------------------------------------------
+glimpse(NHANES)
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"))
+
+select(NHANES, ends_with("Day"))
+
+select(NHANES, contains("Age"))
+
+nahanes_small <- select(
+  NHANES,
+  Age,
+  Gender,
+  BMI,
+  Diabetes,
+  PhysActive,
+  BPSysAve,
+  BPDiaAve,
+  Education
+)
+
+nahanes_small
