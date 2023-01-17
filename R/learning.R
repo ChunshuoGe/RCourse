0a592a1d35f118a568b2009811be49f9773a0896
Chunshuo Ge <Chunshuo@chalmers.se>
2023-01-17 13:50:10 +0100

    exercise of mutating

--- R/learning.R
+++ R/learning.R
@@ -119,3 +119,26 @@ nhanes_updade <- nhanes_small %>%
       "young"
     )
   )
+
+
+# Exercise7-12 ------------------------------------------------------------
+
+
+# 1. BMI between 20 and 40 with diabetes
+nhanes_small %>%
+  # Format should follow: variable >= number or character
+  filter(bmi >= 20 & bmi <= 40 & diabetes == "Yes")
+
+# Pipe the data into mutate function and:
+nhanes_modified <- nhanes_small %>% # Specifying dataset
+  mutate(
+    # 2. Calculate mean arterial pressure
+    mean_arterial_pressure = ((bp_dia_ave *2) + bp_sys_ave )/3,
+    # 3. Create young_child variable using a condition
+    young_child = if_else(
+      age <= 6,
+      "Yes",
+      "No")
+  )
+
+nhanes_modified
